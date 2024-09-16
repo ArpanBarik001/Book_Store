bde405c8083231c0628ae6683e1e02f97d475a02
Arpan Barik <146699772+ArpanBarik001@users.noreply.github.com>
2024-09-16 23:31:24 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 8000;
+export const PORT = 5000;
 
 export const MongodbUrl = 'mongodb+srv://root:root@bookstore.o3puuyo.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore'
